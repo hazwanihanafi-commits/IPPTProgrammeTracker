512e289c84b4b2d8d13908d1a51987a1e7a1b212
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-25 21:26:50 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
-export default { content: ['./index.html','./src/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [] }
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
